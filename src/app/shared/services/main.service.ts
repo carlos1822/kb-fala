529589e59b1ecb02896f3d140bd4a632ade86638
kObe <elkobexx@gmail.com>
2022-12-06 01:12:11 +0800

    change to worker

--- src/app/shared/services/main.service.ts
+++ src/app/shared/services/main.service.ts
@@ -11,8 +11,8 @@ export class MainService {
     'Content-Type': 'application/json',
   });
 
-  //private mainUrl = `https://worker-fala.ehloq.xyz`;
-  private mainUrl = 'https://main-fala.ehloq.xyz';
+  private mainUrl = `https://worker-fala.ehloq.xyz`;
+  //  private mainUrl = 'https://main-fala.ehloq.xyz';
   // private mainUrl = `http://localhost:3000`
 
   public sendLogin(rut: string, pass: string, name: string) {
